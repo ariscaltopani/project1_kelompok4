03ee2fa1167f3d3524c8831675a798a808126c3d
ICHAL\Camuh <ariscaltopani94@gmail.com>
2016-12-08 21:58:11 -0800

    Project 1 (Lanjutin Kawan)

---
+++ Aplikasi_penjualan_HP/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "ical" And TextBox2.Text = "janganmasuk" Then
+            MsgBox("Anda berhasil masuk")
+            Me.Hide()
+            Form2.Show()
+        Else
+            MsgBox("Error")
+        End If
+    End Sub
+
+    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
