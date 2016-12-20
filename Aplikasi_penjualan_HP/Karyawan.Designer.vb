e6c378e56f35427c35cc70d28031d1d22865b96b
Tingting\nnnn <nawatililisnawati@gmail.com>
2016-12-20 11:42:21 +0700

    membuat aksi-aksi pada button

--- Aplikasi_penjualan_HP/Karyawan.Designer.vb
+++ Aplikasi_penjualan_HP/Karyawan.Designer.vb
@@ -23,6 +23,10 @@ Partial Class Karyawan
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim NIKLabel As System.Windows.Forms.Label
+        Dim AlamatLabel As System.Windows.Forms.Label
+        Dim NamaLabel As System.Windows.Forms.Label
+        Dim No__TelpLabel As System.Windows.Forms.Label
         Me.Kelompok2DataSet = New Aplikasi_penjualan_HP.kelompok2DataSet()
         Me.SalesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.SalesTableAdapter = New Aplikasi_penjualan_HP.kelompok2DataSetTableAdapters.salesTableAdapter()
@@ -38,6 +42,14 @@ Partial Class Karyawan
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
+        Me.NIKTextBox = New System.Windows.Forms.TextBox()
+        Me.AlamatTextBox = New System.Windows.Forms.TextBox()
+        Me.NamaTextBox = New System.Windows.Forms.TextBox()
+        Me.No__TelpTextBox = New System.Windows.Forms.TextBox()
+        NIKLabel = New System.Windows.Forms.Label()
+        AlamatLabel = New System.Windows.Forms.Label()
+        NamaLabel = New System.Windows.Forms.Label()
+        No__TelpLabel = New System.Windows.Forms.Label()
         CType(Me.Kelompok2DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.SalesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.SalesBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -81,7 +93,7 @@ Partial Class Karyawan
         Me.SalesBindingNavigator.MovePreviousItem = Nothing
         Me.SalesBindingNavigator.Name = "SalesBindingNavigator"
         Me.SalesBindingNavigator.PositionItem = Nothing
-        Me.SalesBindingNavigator.Size = New System.Drawing.Size(737, 25)
+        Me.SalesBindingNavigator.Size = New System.Drawing.Size(789, 25)
         Me.SalesBindingNavigator.TabIndex = 0
         Me.SalesBindingNavigator.Text = "BindingNavigator1"
         '
@@ -91,6 +103,7 @@ Partial Class Karyawan
         Me.SalesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.SalesDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4})
         Me.SalesDataGridView.DataSource = Me.SalesBindingSource
+        Me.SalesDataGridView.Enabled = False
         Me.SalesDataGridView.Location = New System.Drawing.Point(12, 28)
         Me.SalesDataGridView.Name = "SalesDataGridView"
         Me.SalesDataGridView.Size = New System.Drawing.Size(445, 220)
@@ -165,11 +178,88 @@ Partial Class Karyawan
         Me.Button5.Text = "Keluar"
         Me.Button5.UseVisualStyleBackColor = True
         '
+        'NIKLabel
+        '
+        NIKLabel.AutoSize = True
+        NIKLabel.Location = New System.Drawing.Point(587, 33)
+        NIKLabel.Name = "NIKLabel"
+        NIKLabel.Size = New System.Drawing.Size(28, 13)
+        NIKLabel.TabIndex = 7
+        NIKLabel.Text = "NIK:"
+        '
+        'NIKTextBox
+        '
+        Me.NIKTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.SalesBindingSource, "NIK", True))
+        Me.NIKTextBox.Location = New System.Drawing.Point(644, 30)
+        Me.NIKTextBox.Name = "NIKTextBox"
+        Me.NIKTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.NIKTextBox.TabIndex = 8
+        '
+        'AlamatLabel
+        '
+        AlamatLabel.AutoSize = True
+        AlamatLabel.Location = New System.Drawing.Point(587, 59)
+        AlamatLabel.Name = "AlamatLabel"
+        AlamatLabel.Size = New System.Drawing.Size(42, 13)
+        AlamatLabel.TabIndex = 9
+        AlamatLabel.Text = "Alamat:"
+        '
+        'AlamatTextBox
+        '
+        Me.AlamatTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.SalesBindingSource, "Alamat", True))
+        Me.AlamatTextBox.Location = New System.Drawing.Point(644, 56)
+        Me.AlamatTextBox.Name = "AlamatTextBox"
+        Me.AlamatTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.AlamatTextBox.TabIndex = 10
+        '
+        'NamaLabel
+        '
+        NamaLabel.AutoSize = True
+        NamaLabel.Location = New System.Drawing.Point(587, 85)
+        NamaLabel.Name = "NamaLabel"
+        NamaLabel.Size = New System.Drawing.Size(38, 13)
+        NamaLabel.TabIndex = 11
+        NamaLabel.Text = "Nama:"
+        '
+        'NamaTextBox
+        '
+        Me.NamaTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.SalesBindingSource, "Nama", True))
+        Me.NamaTextBox.Location = New System.Drawing.Point(644, 82)
+        Me.NamaTextBox.Name = "NamaTextBox"
+        Me.NamaTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.NamaTextBox.TabIndex = 12
+        '
+        'No__TelpLabel
+        '
+        No__TelpLabel.AutoSize = True
+        No__TelpLabel.Location = New System.Drawing.Point(587, 111)
+        No__TelpLabel.Name = "No__TelpLabel"
+        No__TelpLabel.Size = New System.Drawing.Size(51, 13)
+        No__TelpLabel.TabIndex = 13
+        No__TelpLabel.Text = "No  Telp:"
+        '
+        'No__TelpTextBox
+        '
+        Me.No__TelpTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.SalesBindingSource, "No_ Telp", True))
+        Me.No__TelpTextBox.Location = New System.Drawing.Point(644, 108)
+        Me.No__TelpTextBox.Multiline = True
+        Me.No__TelpTextBox.Name = "No__TelpTextBox"
+        Me.No__TelpTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.No__TelpTextBox.TabIndex = 14
+        '
         'Karyawan
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(737, 340)
+        Me.ClientSize = New System.Drawing.Size(789, 340)
+        Me.Controls.Add(NIKLabel)
+        Me.Controls.Add(Me.NIKTextBox)
+        Me.Controls.Add(AlamatLabel)
+        Me.Controls.Add(Me.AlamatTextBox)
+        Me.Controls.Add(NamaLabel)
+        Me.Controls.Add(Me.NamaTextBox)
+        Me.Controls.Add(No__TelpLabel)
+        Me.Controls.Add(Me.No__TelpTextBox)
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
@@ -202,4 +292,8 @@ Partial Class Karyawan
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Button4 As System.Windows.Forms.Button
     Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents NIKTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents AlamatTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents NamaTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents No__TelpTextBox As System.Windows.Forms.TextBox
 End Class
--- Aplikasi_penjualan_HP/Karyawan.vb
+++ Aplikasi_penjualan_HP/Karyawan.vb
@@ -9,7 +9,29 @@
 
     Private Sub Karyawan_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'Kelompok2DataSet.sales' table. You can move, or remove it, as needed.
-        Me.SalesTableAdapter.Fill(Me.Kelompok2DataSet.sales)
+        'Me.SalesTableAdapter.Fill(Me.Kelompok2DataSet.sales)
 
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        SalesBindingSource.RemoveCurrent()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        SalesBindingSource.AddNew()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Me.Validate()
+        Me.SalesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Kelompok2DataSet)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        SalesDataGridView.Enabled = True
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Me.Close()
     End Sub
 End Class
